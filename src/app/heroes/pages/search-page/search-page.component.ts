95eaf13cd65b2b5490712286e77247b65acf6cff
Andy Castle <masterchief_and@live.com>
2025-01-28 10:36:25 -0500

    Se añadio el modulo ReactiveFormsModule a heroes.module, para poder usar el formcontrol en nuestro buscador

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SearchPageComponent {
+  constructor(heroresService: HeroesService) {}
 
+  public searchInput = new FormControl('');
 }
