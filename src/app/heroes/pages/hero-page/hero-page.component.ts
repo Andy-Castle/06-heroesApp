0bd2b12a4c4db7dcf49c00fe701ccd9cc7e1e1b1
Andy Castle <masterchief_and@live.com>
2025-01-28 10:04:11 -0500

    En esta sección se vio como obtener el héroe mediante la url y obtener los datos de este cuando le damos a mas

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, type OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interfaces';
 
 @Component({
   selector: 'app-hero-page',
@@ -8,5 +12,24 @@ import { Component, type OnInit } from '@angular/core';
   styles: [],
 })
 export class HeroPageComponent implements OnInit {
-  ngOnInit(): void {}
+  public hero?: Hero;
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((hero) => {
+        if (!hero) return this.router.navigate(['/heroes/list']);
+
+        this.hero = hero;
+        console.log({ hero });
+
+        return;
+      });
+  }
 }
